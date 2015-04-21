53d88f06965d2c8d3fd2f1ae1b2ce1945cffbc04
Anirudh Ramachandran <anirudhr1989@gmail.com>
2015-04-21 11:40:09 -0400

    Capture working, but need to make capture detection better and player-unspecific.

--- play.py
+++ play.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-
+from time import sleep
 
 class Camelot:
-    def __init__(self, color):
+    def __init__(self, color, debug=False):
         self.hu_color = color
+        self.co_color = 'B' if color == 'W' else 'w'
+        self.capmoves = {'W': False, 'B': False}
         self.wp_set = list()
         self.bp_set = list()
         self.p_set = {'B': self.bp_set, 'W': self.wp_set}
@@ -34,6 +36,12 @@ class Camelot:
             self.wp_set.append((4,j))
             self.board[9][j] = 'BP'
             self.bp_set.append((9,j))
+        if debug:
+            self.hu_color = 'W'
+            self.board[4][2], self.board[8][2] = self.board[8][2], self.board[4][2]
+            self.wp_set.remove((4,2))
+            self.wp_set.append((8,2))
+            self.detect_capture('W', 'B', 8, 2)
 
     def printboard(self):
         def horizrule():
@@ -76,46 +84,84 @@ class Camelot:
 
         return simplemoves + cantermoves
 
+    def detect_capture(self, player, target, px, py):
+        for i,j in [(-2,2), (0,2), (2,2), (2,0), (2,-2), (0,-2), (-2,-2), (-2,0)]:
+            if self.isonboard(px+i, py+j):
+                if self.board[px+i][py+j] == '__' and self.board[px+int(i/2)][py+int(j/2)] == target + 'P':
+                    self.capmoves[player] = (i,j, px, py)
+                    return
+        return False
+            #return target + 'P' in [self.board[i][j] \
+                                       #for i in range(fx-1,fx+2) for j in range(fy-1,fy+2) \
+                                       #if self.isonboard(i,j) and (i!=fx or j!=fy)]
+
     def hu_makemove(self):
-        for idx, elem in enumerate(self.p_set[self.hu_color]):
-            print('*** %i: %s' % (idx, elem))
-        choice1 = -1
-        while choice1 not in range(6):
-            choice1 = input('Select your piece from the above: ')
-            if not choice1.isdigit():
-                print('Bad choice, try again. ', end='')
-            else:
-                choice1 = int(choice1)
-                if choice1 not in range(6):
+        if self.capmoves[self.hu_color] == False:
+            for idx, elem in enumerate(self.p_set[self.hu_color]):
+                print('*** %i: %s' % (idx, elem))
+            choice1 = -1
+            while choice1 not in range(6):
+                choice1 = input('Select your piece from the above: ')
+                if not choice1.isdigit():
                     print('Bad choice, try again. ', end='')
-        px, py = self.p_set[self.hu_color][choice1]
-        moves = self.getmoves(px, py, self.hu_color)
-        for idx, elem in enumerate(moves):
-            print('*** %i: %s' % (idx, elem))
-        choice2 = -1
-        while choice2 not in range(len(moves)):
-            choice2 = input('Select your move from the above: ')
-            if not choice2.isdigit():
-                print('Bad choice, try again. ', end='')
-            else:
-                choice2 = int(choice2)
-                if choice2 not in range(len(moves)):
+                else:
+                    choice1 = int(choice1)
+                    if choice1 not in range(6):
+                        print('Bad choice, try again. ', end='')
+            px, py = self.p_set[self.hu_color][choice1]
+            moves = self.getmoves(px, py, self.hu_color)
+            for idx, elem in enumerate(moves):
+                print('*** %i: %s' % (idx, elem))
+            choice2 = -1
+            while choice2 not in range(len(moves)):
+                choice2 = input('Select your move from the above: ')
+                if not choice2.isdigit():
                     print('Bad choice, try again. ', end='')
-        fx, fy = moves[choice2]
-        self.p_set[self.hu_color][choice1] = (fx, fy)
-        self.board[fx][fy] = self.board[px][py]
-        self.board[px][py] = '__'
+                else:
+                    choice2 = int(choice2)
+                    if choice2 not in range(len(moves)):
+                        print('Bad choice, try again. ', end='')
+            fx, fy = moves[choice2]
+            self.p_set[self.hu_color][choice1] = (fx, fy)
+            self.board[fx][fy] = self.board[px][py]
+            self.board[px][py] = '__'
+            #now we check for captures and set a flag for later use
+            self.detect_capture(self.hu_color, self.co_color, fx, fy)
+        else:
+            print('Playing capture move.')
+            sleep(1)
+            i, j, px, py = self.capmoves[self.hu_color]
+            self.capmoves[self.hu_color] = False
+            fx, fy = px+i, py+j
+            ex, ey = px+int(i/2), py+int(j/2)
+            self.p_set[self.hu_color][self.p_set[self.hu_color].index((px, py))] = (fx, fy)
+            self.board[fx][fy] = self.board[px][py]
+            self.board[px][py] = '__'
+            self.p_set[self.co_color].remove((ex, ey))
+            self.board[ex][ey] = '__'
+            self.detect_capture(self.hu_color, self.co_color, fx, fy)
+
+    def co_makemove(self):
+        piecemoves = dict()
+        for px, py in self.p_set[self.co_color]:
+            piecemoves[(px, py)] = self.getmoves(px, py, self.co_color)
 
 if __name__ == '__main__':
+    #while True:
+    #    color = input('Select your color (W/B): ')
+    #    if color not in {'W', 'B'}:
+    #        print('Bad input, try again.')
+    #    else:
+    #        break
+    color = 'W'
+    game = Camelot(color, debug=True)
+    if color == 'W':
+        game.printboard()
+        game.hu_makemove()
     while True:
-        color = input('Select your color (W/B): ')
-        if color not in {'W', 'B'}:
-            print('Bad input, try again.')
-        else:
-            break
-    game = Camelot(color)
-    game.printboard()
-    game.hu_makemove()
-    game.printboard()
+        game.co_makemove()
+        game.printboard()
+        game.hu_makemove()
+        game.printboard()
 #print(game.p_set['B'])
 #print(game.p_set['W'])
